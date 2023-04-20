6679f2c39ef014ab2c8aaf350476a6c1cf8d5dd9
Alena <weaterwind@gmail.com>
2023-04-20 17:35:39 +0700

    Task 66 done

--- Task_66/Program.cs
+++ Task_66/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumNumbers(int number1, int number2)
+{
+    if (number1 == number2)
+        return number1;
+    return SumNumbers(number1, number2 - 1) + number2;
+}
+
+Console.Clear();
+Console.Write("Введите 2 числа через пробел: ");
+int[] mn = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+Console.WriteLine(SumNumbers(mn[0], mn[1]));
